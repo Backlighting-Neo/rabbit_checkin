38cf7dffb00c332619925ac707d868a45628cfcd
KevinTang <tjtzc@126.com>
2018-12-09 14:57:01 +0800

    fix

--- proxy/rule/fake_checkin.js
+++ proxy/rule/fake_checkin.js
@@ -50,11 +50,11 @@ module.exports = {
     const {requestOptions} = requestDetail;
     const {hostname, path} = requestOptions;
 
-    if(hostname === secret.host && path === secret.api.api1) {
+    if(hostname === secret.host && path.startsWith(secret.api.api1)) {
       return handleApi1(requestDetail);
     }
 
-    if(hostname === secret.host && path === secret.api.api2) {
+    if(hostname === secret.host && path.startsWith(secret.api.api2)) {
       return handleApi2(requestDetail);
     }
 
